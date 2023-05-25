179bdb46eaaf39be78443dd05393b4f09d7a7c94
iamchaze <virajkale9604@gmail.com>
2023-05-25 21:55:16 +0530

    updated code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { QuizComponent } from './components/quiz/quiz.component';
 import { ResultComponent } from './components/result/result.component';
 import { QuestionComponent } from './components/question/question.component';
-import { authGuard } from './shared/guard/auth.guard';
 
 const routes: Routes = [
   {path:"", component:QuizComponent},
--- src/app/shared/guard/auth.guard.ts
+++
@@ -1,9 +0,0 @@
-import { CanActivateFn } from '@angular/router';
-
-export const authGuard: CanActivateFn = (route, state) => {
-  if(localStorage.getItem('userid') || sessionStorage.getItem('userid')){
-    return true
-  } else {
-    return false
-  }
-};
